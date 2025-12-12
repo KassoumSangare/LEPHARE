baa5997a2a4e59ba7c9a82803863c2b2090fd780
Ouattara Dro <ouattaradro@gmail.com>
2025-12-12 09:36:35 +0000

    Set default page size to 200

--- uranus/settings.py
+++ uranus/settings.py
@@ -145,7 +145,7 @@ REST_FRAMEWORK = {
     # ],
     # "DEFAULT_MODEL_SERIALIZER_CLASS": "drf_toolbox.serializers.ModelSerializer",
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 20,
+    "PAGE_SIZE": 200,
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
         "rest_framework.filters.SearchFilter",
